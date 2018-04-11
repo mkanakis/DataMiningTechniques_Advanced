72898d03e7065fa662776b1053854d5f59609181
Mick-IJ <Mick.ijzer@gmail.com>
2018-04-11 10:53:29 +0200

    Update Useful+features+identification.py
    Changed it so that the mood from the day in the past is also used to predict mood in the future

--- Useful+features+identification.py
+++ Useful+features+identification.py
@@ -7,21 +7,29 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import pearsonr
+from collections import OrderedDict
+
+id_set = list(OrderedDict.fromkeys(cleandata['id']))
+
+#create a base for the dataframe (including the future mood variable)
+persondf = cleandata[cleandata['id'] == 'AS14.02']
+persondf['mood_f'] = persondf['mood].shift(1)
+
 
 #find the correlations between the mood on a day and the variables i days ago
 for i in range(1, 6):
-    testdf =  pd.DataFrame(columns = cleandata.columns)
+    testdf =  pd.DataFrame(columns = persondf.columns)
     print('\t\tOFFSET:', i, 'day(s) ago\n')
     #shift the values for mood so that the variables of a day are followed by the mood of a day in the future
     for person in id_set:
         persondf = cleandata[cleandata['id'] == person]
-        persondf['mood'] = persondf['mood'].shift(-i)
+        persondf['mood_f'] = persondf['mood'].shift(-i)
         testdf = testdf.append(persondf)
-    testdf = testdf[testdf['mood'].notnull()]
+    testdf = testdf[testdf['mood_f'].notnull()]
     
     #test the correlation between mood and the other variables
-    y = testdf['mood']
-    x = testdf.drop('mood', 1)
+    y = testdf['mood_f']
+    x = testdf.drop('mood_f', 1)
 
     print('\tSIGNIFICANT CORRELATIONS:\n')
     for variable in x.columns[2:]:

f472d68d47f0a288ebff3d7ebe81bdcd89ef84d3
Mick-IJ <Mick.ijzer@gmail.com>
2018-04-13 18:58:03 +0200

    Add files via upload
    To test several hyperparameters for different models, and evaluate the performance on the test set

---
+++ Hyperparameter+selection+and+validation.py
@@ -0,0 +1,87 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+from sklearn.linear_model import LinearRegression
+import math
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from xgboost import XGBRegressor
+
+#hyperparameter selection
+tree_ = DecisionTreeRegressor()
+forest_ = RandomForestRegressor()
+xgb = XGBRegressor()
+
+tuned_parameters_tree = [{'min_samples_leaf': range(35,46,5),'max_depth': range(45,66,5), 'max_features': range(50, 56, 1)}]
+tuned_parameters_forest = [{'n_estimators': range(20,31,5), 'min_samples_leaf': range(10,31,5),'max_depth': range(10,21,5), 'max_features': range(45,52, 2)}]
+tuned_parameters_xgb = [{'max_depth': range(1, 5, 2), 'n_estimators': range(70, 86, 5), 'learning_rate': [.125, .15, .175]}]
+
+
+models = [(tuned_parameters_tree, tree_, 'tree'), (tuned_parameters_forest, forest_, 'forest'), (tuned_parameters_xgb, xgb, 'xgb')]
+
+best_models = list()
+
+for tuned_parameters, model, modelname in models: 
+    scores = ['neg_mean_squared_error']
+    for score in scores:
+        clf = GridSearchCV(model, tuned_parameters, cv=10, scoring=score)
+        clf.fit(x_train, y_train)
+       
+        print('----------------------------------------------------------------------')
+        print("Gridsearch for", modelname, "with", score, "as scoring method:")
+        print()
+        print("Best parameters set found on development set:")
+        print()
+        print(clf.best_params_, 'with score', clf.best_score_.round(3))
+        print()
+        print()
+        print("Grid scores on development set:")
+        print()
+        means = clf.cv_results_['mean_test_score']
+        stds = clf.cv_results_['std_test_score']
+        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+            print(mean.round(3), std.round(3), params)
+        print()
+        print()
+        best_models.append((clf.best_score_.round(3), modelname, score, clf.best_params_))
+
+
+predictionlist = list()
+for modeldata in best_models:
+    sme, model, error, params = modeldata
+    if model == 'tree':
+        treemodel = DecisionTreeRegressor(**params)
+        treemodel.fit(x_train, y_train)
+        tree_predict = treemodel.predict(x_test)
+        predictionlist.append((model, tree_predict))
+    if model == 'forest':
+        forestmodel = RandomForestRegressor(**params)
+        forestmodel.fit(x_train, y_train)
+        forest_predict = forestmodel.predict(x_test)
+        predictionlist.append((model, forest_predict))
+    if model == 'xgb':
+        xgbmodel = XGBRegressor(**params)
+        xgbmodel.fit(x_train,y_train)
+        xgb_predict = xgbmodel.predict(x_test)
+        predictionlist.append((model, xgb_predict))
+
+base_prediction = x_test['moodPrevDay']
+predictionlist.append(('baseline', base_prediction)) 
+
+lin_reg = LinearRegression()
+lin_reg.fit(x_train, y_train)
+lin_prediction = lin_reg.predict(x_test)
+predictionlist.append(('linear', lin_prediction))
+
+for predictor, prediction in predictionlist:
+    
+    errors = y_test.subtract(prediction)
+    squared_errors = errors*errors
+    print('\n\t\t' + predictor.upper() + '\n')
+    print('\tMSE:', squared_errors.mean(), 'SD', squared_errors.std())
+    print('\tRMSE:', math.sqrt(squared_errors.mean()), 'SD:', math.sqrt(squared_errors.std()))
+
